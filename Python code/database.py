e5c23f89f2f309e00364c319678997a173c5d64f
pllee4 <42335542+pllee4@users.noreply.github.com>
2019-12-18 18:46:14 +0800

    Update on pydoc

--- Python code/database.py
+++ Python code/database.py
@@ -9,22 +9,32 @@ c = conn.cursor()
 dataFromFirebase = [ ]
     
 
-def returnAttributes():
-    return (22, 'Male', 'Yes', 3,
-            'E:\Academic Year 4 201920\Sem7\Software Engineering\Project1')
-
 
 class VoiceTable():
+    """
+    The class is written to for creating of VoiceTable in database.
+    """ 
     
     def __init__(self):
+        """
+        This method initialize the instances of VoiceTable.
+        """   
         self.createVoiceTable()
         
     def createVoiceTable(self):
+        """
+        This method create the VoiceTable with VoiceId (Integer Primary Key) and Voice (Text Unique).
+        """  
         c.execute('''CREATE TABLE IF NOT EXISTS VoiceTable(VoiceId INTEGER PRIMARY KEY,
                      Voice TEXT UNIQUE)''')
 
     def insertToVoiceTable(self, voice):
-        # for index, value in enumerate(command, start = 1):
+        """
+        This method is to insert the Voice into VoiceTable.
+        
+        Parameters:
+        voice (List): the list of voice would be inserted into the VoiceTable if it is not existed in VoiceTable
+        """
         self.voice = voice
         for value in self.voice:
             c.execute('''INSERT OR IGNORE INTO VoiceTable(Voice)
@@ -32,16 +42,32 @@ class VoiceTable():
         conn.commit()
     
     def getVoice(self):
+        
+        """
+        This method is to return the list of voice in VoiceTable
+        
+        Returns:
+        list: the list of voice that is inserted into VoiceTable
+        """
         return self.voice
 
         
 def createTable():
+    """
+    This method is to create table of CrowdSourcingMandarin using sqlite3 command
+    """
     c.execute('''CREATE TABLE IF NOT EXISTS CrowdSourcingMandarin (ID INTEGER PRIMARY KEY, 
                  Age INTEGER, Gender TEXT, NativeSpeaker TEXT, VoiceId INTEGER, DownloadLink TEXT UNIQUE,
                  FOREIGN KEY (VoiceId) REFERENCES VoiceTable (VoiceId)
                  )''')
 
 def insertData(dataFromFirebase):
+    """
+    This method is to insert data from Firebase into table of CrowdSourcingMandarin using sqlite3 command
+    
+    Parameters:
+    dataFromFirebase(list): the list of data which are consisted of Age, Gender, NativeSpeaker, VoiceId, DownloadLink
+    """ 
     c.executemany('''INSERT OR IGNORE INTO CrowdSourcingMandarin(Age, Gender, NativeSpeaker, VoiceId, DownloadLink)
                      VALUES (?, ?, ?, ?, ?)''',
                 dataFromFirebase)
@@ -49,6 +75,9 @@ def insertData(dataFromFirebase):
 
 
 def init():
+    """
+    This method is to create instance of VoiceTable, insert the data into VoiceTable and create table of CrowdSourcingMandarin 
+    """
     MandarinTable = VoiceTable()
     MandarinTable.insertToVoiceTable(voice.voice)
     createTable()
